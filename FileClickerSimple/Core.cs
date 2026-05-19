dc66b6203afe5f97b69282cb5263cd19a8dee7d2
GET Academy <terje@kolderup.net>
2026-05-19 09:33:11 +0200

    Initial commit

---
+++ FileClickerSimple/Core.cs
@@ -0,0 +1,26 @@
+﻿namespace FileClickerSimple
+{
+    // DomainModel
+    record HandleKeyResult(int Count, Action Action);
+
+    enum Action {
+        NoAction,
+        ApplicationExit,
+        SaveToFile,
+    }
+
+    // ApplicationServices
+    internal class ClickerService
+    {
+        public static HandleKeyResult HandleKey(ConsoleKey key, int count)
+        {
+            return key switch
+            {
+                ConsoleKey.Escape => new HandleKeyResult(count, Action.ApplicationExit),
+                ConsoleKey.Spacebar => new HandleKeyResult(count + 1, Action.SaveToFile),
+                ConsoleKey.R => new HandleKeyResult(0, Action.SaveToFile),
+                _ => new HandleKeyResult(count, Action.NoAction)
+            };
+        }
+    }
+}
